8b678cd2f8461bd4abc2b5dcfeaa12a383ee5e09
VikramjitSinghRathee <vratheeapps@gmail.com>
2018-04-25 21:29:25 -0400

    piechart with data from shared pref and xml info

--- app/src/main/java/com/example/vrathee/myapplication/Main7Activity.java
+++ app/src/main/java/com/example/vrathee/myapplication/Main7Activity.java
@@ -1,7 +1,10 @@
 package com.example.vrathee.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
@@ -12,18 +15,87 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.stream.DoubleStream;
 
 public class Main7Activity extends AppCompatActivity {
 
-    float rand_values[] = {98.8f,123.8f,161.6f,24.2f,52f};
-    String randNames[] = {"swag","mvp","bling","trill","best"};
+    float rand_values[] = {0,0,0,0,0};
+    String randNames[] = {"default","default","default","default","default"};
+
+    Map<Integer, String> map=new HashMap<Integer, String>();
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
 
+        sharedPreferences = getApplicationContext().getSharedPreferences("value_scale", Context.MODE_PRIVATE);
+        Map<String,?> keys = sharedPreferences.getAll();
+
+        for(Map.Entry<String,?> entry : keys.entrySet()){
+            map.put(Integer.parseInt(entry.getValue().toString()),entry.getKey());
+        }
+
+        /* FOR TESTING
+
+        for(Map.Entry<String,Integer> entrys : map.entrySet()){
+            Log.d("nops values",entrys.getKey() + ": " +
+                    entrys.getValue().toString());
+        }
+
+
+        Set set = map.entrySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()) {
+            Map.Entry me = (Map.Entry)iterator.next();
+            Log.d("before values",me.getKey() + ": " +
+                    me.getValue());
+        }
+
+        Set set2 = Tmap.entrySet();
+        Iterator iterator2 = set2.iterator();
+        while(iterator2.hasNext()) {
+            Map.Entry me2 = (Map.Entry)iterator2.next();
+            Log.d("after values",me2.getKey() + ": " +
+                    me2.getValue().toString());
+        }*/
+
+
+        // GET ONLY THE TOP 5 VALUES
+        Map<Integer, String> Tmap = new TreeMap<Integer, String>(map);
+        ArrayList<Integer> newkey = new ArrayList<Integer>(Tmap.keySet());
+        int jj = 0;
+        for(int i=newkey.size()-1; i>=0;i--){
+            Log.d("after2 values",Tmap.get(newkey.get(i)));
+
+            // GET ONLY THE TOP 5 VALUES
+            randNames[jj]= Tmap.get(newkey.get(i));
+            rand_values[jj]= (float) newkey.get(i);
+            jj++;
+            if (i<=newkey.size()-5){
+                break;
+            }
+        }
+
+        // UPDATING THE ARRAY OF VALUES GOING INTO PIECHART RELATIVE TO TOTAL SUM
+        float val_sum = 0;
+        for (float element : rand_values) {
+            val_sum += element;
+        }
+        for (int x =0;x <rand_values.length;x++){
+            rand_values[x] = rand_values[x] / val_sum * 100; // redundant but will leave it here as of now
+        }
+
+
+        // SETUP PIE CHART
         setupPieChart();
     }
 
--- app/src/main/java/com/example/vrathee/myapplication/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/vrathee/myapplication/RecyclerViewAdapter.java
@@ -1,6 +1,8 @@
 package com.example.vrathee.myapplication;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
@@ -14,13 +16,21 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
 
     private static final String TAG = "RecyclerViewAdapter";
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     private ArrayList<String> mImageNames = new ArrayList<>();
     private ArrayList<String> mImage = new ArrayList<>();
@@ -55,21 +65,50 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             public void onClick(View v) {
                 Log.d(TAG, "onClick: clicked on " + mImageNames.get(position));
                 Toast.makeText(mcontext, mImageNames.get(position),Toast.LENGTH_SHORT).show();
+
+            // UPDATING OF THE DATA AFTER CLICKING ON THE VIEW
+
+                // this is the date generation for naming the file
+                Date c = Calendar.getInstance().getTime();
+                SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+                String formattedDate = df.format(c);
+
+
+
+                // OBTAINING OLD DATA AND SAVING THE NEW ONE
+                sharedPreferences = mcontext.getSharedPreferences("value_scale", Context.MODE_PRIVATE);
+                editor = sharedPreferences.edit();
+
+                // we have to remove these so that old data is not deleted
+                //editor.clear();
+                //editor.apply();
+
+                Toast.makeText(mcontext,"the sharedpreference name is "+"value_scale", Toast.LENGTH_LONG).show();
+                int old_scale_value = sharedPreferences.getInt(mImageNames.get(position),0);
+                setSharedPreferences(mImageNames.get(position), old_scale_value);
+
+                // FOR TESTING PURPOSES
+                Toast.makeText(mcontext,"Data saved", Toast.LENGTH_LONG).show();
+                int cur_value = sharedPreferences.getInt(mImageNames.get(position),0);
+                Toast.makeText(mcontext,"the data loaded is "+String.valueOf(cur_value), Toast.LENGTH_LONG).show();
             }
         });
 
         holder.parentLayout.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                // MAYBE THINK ABOUT REPLACING SNACKBAR WITH A DIALOG ??
 
-                final Snackbar snackbar = Snackbar.make(v, "his is a totally random paragraph that doesnt " +
-                        "actually mean anything, and is really talking about " +
-                        "the random paragraph, there isnt much to worry sbout " +
-                        "because its just a random paragraph, don’t worry, " +
-                        "everything will be ok when random paragraph is by your side! " +
-                        "Random paragraph is a completely random typed text, hense the " +
-                        "name ‘random paragraph’.  "+mImageNames.get(position), Snackbar.LENGTH_INDEFINITE);
+                Resources res = mcontext.getResources();
+                String valueNameInfo = mImageNames.get(position);
+
+                // deftype string looks for name in the strings.xml. this is important, for drawable it's drawable
+                int resID = mcontext.getResources().getIdentifier(valueNameInfo,"string", mcontext.getPackageName());
+                String infotext = res.getString(resID)+ mImageNames.get(position);;
+
+                // we can get correct info for the position clicked by editing strings.xml and
+                // naming each string in strings.xml corresponding to mImageNames.get(position)
+
+                final Snackbar snackbar = Snackbar.make(v,infotext, Snackbar.LENGTH_INDEFINITE);
                 View snackbarView = snackbar.getView();
                 TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
                 textView.setMaxLines(20);  // show multiple line
@@ -83,6 +122,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                 return true;
             }
         });
+
+        // for info need to load correct info from xml file
+        // for time IMPLEMENT SOMETHING SINCE UTC
+        // FOR THE NAME OF THE VALUE LIST IMPLEMENT SINGLE NAME INSTEAD OF FORMATTED DATE NAME
+        // 1. for now open shared preference here single file and keep appending data to it based on clicks
+        // 2. after 1 week (make a code to check maybe have it in the name of the file also), delete the old file and create a new file
+        // 3. now all the keys are 0 and data from clicks are getting appended to that until next week.
+        // 4. load the top 5 keys from the shared pref and plot the pie chart
     }
 
     @Override
@@ -103,5 +150,22 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             parentLayout = itemView.findViewById(R.id.parent_layout);
 
         }
+    }
+
+    public void setSharedPreferences(String name, int old_scale_value){
+        editor = sharedPreferences.edit();
+        //editor.clear();
+        int scale_value;
+        if(old_scale_value != 0)
+        {
+            scale_value =  old_scale_value + 5;
+            editor.putInt(name,scale_value);
+        }
+        else
+        {
+            editor.putInt(name, 5);
+        }
+        //editor.commit();
+        editor.apply();
     }
 }
